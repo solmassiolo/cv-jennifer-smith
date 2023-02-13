4045d4bf3e31884524f249ac7605ae4e48ffa137
Sol Massiolo <solmassiolo@gmail.com>
2023-02-13 13:23:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const typed = new Typed('.typed', {
+	stringsElement: '#cadenas-texto', 
+	typeSpeed: 75, 
+	startDelay: 300, 
+	backSpeed: 75, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1500, 
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
+});
